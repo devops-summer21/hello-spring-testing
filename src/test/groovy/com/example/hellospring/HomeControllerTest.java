a68e28d5a566e85878b7b46b0f3ec0b92bebb7e1
Javier Arturo Rodriguez <jrodriguez@sinensia.com>
2021-08-04 11:57:07 +0200

    Fixed hola unit test.

--- src/test/groovy/com/example/hellospring/HomeControllerTest.java
+++ src/test/groovy/com/example/hellospring/HomeControllerTest.java
@@ -1,14 +1,27 @@
 package com.example.hellospring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void hola() {
+        assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/hola", String.class))
+                .isEqualTo("Hola ke ase");
+
     }
 }
